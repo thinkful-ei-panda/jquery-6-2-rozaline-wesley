faab02ada804db7727045635e66f331ff3708f91
Wesley Rou <wesleyrou@gmail.com>
2020-06-02 11:50:52 -0700

    changes

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ $('.thumbnails').on('click','.thumbnail', event => {
   event.preventDefault();
     // take attribute and alt of thumbnail image
     // edit hero class to be thumbnail image
-    // $('.hero').attr('src',$(event).attr('src'));
-    $('.hero').attr('alt','bloop');
-    // console.log(event);
+    const imgSrc = $(event.currentTarget).find('img').attr('src');  
+    const imgAlt = $(event.currentTarget).find('img').attr('alt');
 
-//   console.log('delete button clicked');
+    $('.hero').find('img').attr('alt',imgAlt);
+    $('.hero').find('img').attr('src',imgSrc);
+    // console.log($('.hero').attr('alt'));
+
+  console.log($(event.currentTarget).find('img'));
 });
